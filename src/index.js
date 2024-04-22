86c017e35a498015686502845e4526551f1f7085
Logan Grasby <logangrasby@gmail.com>
2024-04-22 12:35:15 -0600

    init

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 import HTML from "./index.html";
 const app = {
-	async chat(request, env) {
-		const formData = await request.formData();
-		const prompt = formData.get("prompt");
-		const imageFile = formData.get("image");
+	async chat(request, env, retryCount = 0) {
+		try {
+			const formData = await request.formData();
+			const prompt = formData.get("prompt");
+			const imageFile = formData.get("image");
 
-		const inputs = {
-			prompt: prompt,
-			max_tokens: 2000,
-		};
-		const modelId = '@cf/llava-hf/llava-1.5-7b-hf';
-		inputs.prompt = `
-		USER: ${prompt}  
-		ASSISTANT:`
-		const imageArrayBuffer = await imageFile.arrayBuffer();
-		inputs.image = [...new Uint8Array(imageArrayBuffer)];
-		const response = await env.AI.run(modelId, inputs);
-		return new Response(response.description)
+			const inputs = {
+				prompt: prompt,
+				max_tokens: 2000,
+			};
+			const modelId = '@cf/llava-hf/llava-1.5-7b-hf';
+			inputs.prompt = `
+			USER: ${prompt}  
+			ASSISTANT:`
+			const imageArrayBuffer = await imageFile.arrayBuffer();
+			inputs.image = [...new Uint8Array(imageArrayBuffer)];
+			const response = await env.AI.run(modelId, inputs);
+			return new Response(response.description)
+		} catch (error) {
+			if (retryCount < 3) {
+				return this.chat(request, env, retryCount + 1);
+			} else {
+				throw error;
+			}
+		}
 	},
 	html() {
 		return new Response(HTML, { headers: { "Content-Type": "text/html;charset=UTF-8" } });
